283b345ca26a1e61e656ce26439ea7da1dfa81ef
Surya Suresh <107568033+SuryaSuresh18@users.noreply.github.com>
2023-02-27 17:42:30 +0530

    Add files via upload

---
+++ practicals/StringExample.java
@@ -0,0 +1,17 @@
+package my;
+
+public class StringExample {
+	public static void main(String[] args) {
+		char[]names = {'S','u','r','y','a'};
+		String s = new String(names);
+		System.out.print(s);
+		//or 
+		String n="Suresh";
+		System.out.println(" "+n);
+		//literals
+		String a = new String("Anudip Foundation");
+		System.out.println(a);
+		
+	}
+
+}
